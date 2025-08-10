b837e58231fdbd31bb070248c7a2ba35d600e375
gulmix <gulmix@bk.ru>
2025-08-10 20:45:24 +0300

    Fixing CI workflow

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-const connString = "postgresql://postgres:psql@172.19.34.230:5432/bank?sslmode=disable"
+const connString = "postgresql://postgres:psql@localhost:5432/bank?sslmode=disable"
 
 var testQueries *Queries
 var testDB *pgxpool.Pool
