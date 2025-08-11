cc91662eae6c54fa9d0594f76b1cd0e8b630b0bc
gulmix <gulmix@bk.ru>
2025-08-11 21:53:10 +0300

    Fixing API tests

---
+++ util/config.go
@@ -0,0 +1,21 @@
+package util
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBSource   string `mapstructure:"CONN_STRING"`
+	ServerName string `mapstructure:"SERVER_NAME"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
